2618b5ff4f8c53543aa113ab3075420b2b31c0a5
lee-ji-an <leeja0407@naver.com>
2022-12-19 01:04:05 +0900

    Feat: TeamProject로 프로젝트 병합 및 메인페이지 연결

---
+++ TeamProject/app/src/main/java/com/example/teamproject/MainActivity.kt
@@ -0,0 +1,158 @@
+package com.example.teamproject
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.CalendarView
+import android.widget.CalendarView.OnDateChangeListener
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.teamproject.databinding.ActivityMainBinding
+import java.io.FileInputStream
+import java.io.FileOutputStream
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.travelRecommend.setOnClickListener {
+            val intent: Intent = Intent(this, CalendarActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+
+    /*
+    var fname: String? = null
+    var str: String? = null
+    var calendarView: CalendarView? = null
+    var cha_Btn: Button? = null
+    var del_Btn: Button? = null
+    var save_Btn: Button? = null
+    var diaryTextView: TextView? = null
+    var textView2: TextView? = null
+    var textView3: TextView? = null
+    var contextEditText: EditText? = null
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        calendarView = findViewById(R.id.calendarView)
+        diaryTextView = findViewById(R.id.diaryTextView)
+        save_Btn = findViewById(R.id.save_Btn)
+        del_Btn = findViewById(R.id.del_Btn)
+        cha_Btn = findViewById(R.id.cha_Btn)
+        textView2 = findViewById(R.id.textView2)
+        textView3 = findViewById(R.id.textView3)
+        contextEditText = findViewById(R.id.contextEditText)
+        //로그인 및 회원가입 엑티비티에서 이름을 받아옴
+        val intent = intent
+        val name = intent.getStringExtra("userName")
+        val userID = intent.getStringExtra("userID")
+        textView3.setText(name + "님의 달력 일기장")
+        calendarView.setOnDateChangeListener(OnDateChangeListener { view, year, month, dayOfMonth ->
+            diaryTextView.setVisibility(View.VISIBLE)
+            save_Btn.setVisibility(View.VISIBLE)
+            contextEditText.setVisibility(View.VISIBLE)
+            textView2.setVisibility(View.INVISIBLE)
+            cha_Btn.setVisibility(View.INVISIBLE)
+            del_Btn.setVisibility(View.INVISIBLE)
+            diaryTextView.setText(String.format("%d / %d / %d", year, month + 1, dayOfMonth))
+            contextEditText.setText("")
+            checkDay(year, month, dayOfMonth, userID)
+        })
+        save_Btn.setOnClickListener(View.OnClickListener {
+            saveDiary(fname)
+            str = contextEditText.getText().toString()
+            textView2.setText(str)
+            save_Btn.setVisibility(View.INVISIBLE)
+            cha_Btn.setVisibility(View.VISIBLE)
+            del_Btn.setVisibility(View.VISIBLE)
+            contextEditText.setVisibility(View.INVISIBLE)
+            textView2.setVisibility(View.VISIBLE)
+        })
+    }
+
+    fun checkDay(cYear: Int, cMonth: Int, cDay: Int, userID: String?) {
+        fname = "" + userID + cYear + "-" + (cMonth + 1) + "" + "-" + cDay + ".txt" //저장할 파일 이름설정
+        var fis: FileInputStream? = null //FileStream fis 변수
+        try {
+            fis = openFileInput(fname)
+            val fileData = ByteArray(fis.available())
+            fis.read(fileData)
+            fis.close()
+            str = String(fileData)
+            contextEditText!!.visibility = View.INVISIBLE
+            textView2!!.visibility = View.VISIBLE
+            textView2!!.text = str
+            save_Btn!!.visibility = View.INVISIBLE
+            cha_Btn!!.visibility = View.VISIBLE
+            del_Btn!!.visibility = View.VISIBLE
+            cha_Btn!!.setOnClickListener {
+                contextEditText!!.visibility = View.VISIBLE
+                textView2!!.visibility = View.INVISIBLE
+                contextEditText!!.setText(str)
+                save_Btn!!.visibility = View.VISIBLE
+                cha_Btn!!.visibility = View.INVISIBLE
+                del_Btn!!.visibility = View.INVISIBLE
+                textView2!!.text = contextEditText!!.text
+            }
+            del_Btn!!.setOnClickListener {
+                textView2!!.visibility = View.INVISIBLE
+                contextEditText!!.setText("")
+                contextEditText!!.visibility = View.VISIBLE
+                save_Btn!!.visibility = View.VISIBLE
+                cha_Btn!!.visibility = View.INVISIBLE
+                del_Btn!!.visibility = View.INVISIBLE
+                removeDiary(fname)
+            }
+            if (textView2!!.text == null) {
+                textView2!!.visibility = View.INVISIBLE
+                diaryTextView!!.visibility = View.VISIBLE
+                save_Btn!!.visibility = View.VISIBLE
+                cha_Btn!!.visibility = View.INVISIBLE
+                del_Btn!!.visibility = View.INVISIBLE
+                contextEditText!!.visibility = View.VISIBLE
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    @SuppressLint("WrongConstant")
+    fun removeDiary(readDay: String?) {
+        var fos: FileOutputStream? = null
+        try {
+            fos = openFileOutput(readDay, MODE_NO_LOCALIZED_COLLATORS)
+            val content = ""
+            fos.write(content.toByteArray())
+            fos.close()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    @SuppressLint("WrongConstant")
+    fun saveDiary(readDay: String?) {
+        var fos: FileOutputStream? = null
+        try {
+            fos = openFileOutput(readDay, MODE_NO_LOCALIZED_COLLATORS)
+            val content = contextEditText!!.text.toString()
+            fos.write(content.toByteArray())
+            fos.close()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+     */
+}
+
---
+++ TeamProject/app/src/main/java/com/example/teamproject/SplashActivity.kt
@@ -0,0 +1,52 @@
+package com.example.teamproject
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+            finish()
+        },DURATION)
+
+    }
+    companion object {
+        private const val DURATION : Long = 3000
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+    }
+//        // 일정 시간 지연 이후 실행하기 위한 코드
+//        Handler(Looper.getMainLooper()).postDelayed({
+//
+//            // 일정 시간이 지나면 MainActivity로 이동
+//            val intent= Intent( this,MainActivity::class.java)
+//            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+//            startActivity(intent)
+//
+//            // 이전 키를 눌렀을 때 스플래스 스크린 화면으로 이동을 방지하기 위해
+//            // 이동한 다음 사용안함으로 finish 처리
+//            finish()
+//        },DURATION)
+//    }
+//    companion object
+//    {
+//        private const val DURATION : Long = 3000
+//    }
+//
+//    override fun onBackPressed()
+//    {
+//        super.onBackPressed()
+//    }
+
+}
